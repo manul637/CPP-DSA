b05a54e85e1d7d4287c0f8c6e314cad5525eba5e
manul <manulgupta1009@gmail.com>
2026-01-11 14:08:12 +0530

    Pointer in C++

--- Pointer.cpp
+++ Pointer.cpp
@@ -9,13 +9,15 @@ using namespace std;
 
 void update(int *p){
 *p = 20;
-    
-}
+    }
+
+
 //pass by refrence using alias
-void update2(int &b){                       //here and is alias
+
+void update2(int &b){                       //here "&" is alias
 b = 30;
-    
-}
+    }
+
 
 int main() {
 //     int a = 10;
@@ -26,6 +28,8 @@ int main() {
 // cout << "Value using single pointer: " << *ptr << endl; 
 // cout << "Value using double pointer: " << **pptr << endl;
 
+// ---------------------------------------------------------------------------
+
 int a = 10;
 
 cout << "Before update: " << a << endl;
@@ -33,6 +37,23 @@ update(&a);
 cout << "After update using pointer: " << a << endl;
 update2(a);
 cout << "After update using reference: " << a << endl;
+
+// -----------------------------------------------------------------------------
+
+int arr[5] = {1, 2, 3, 4, 5};
+
+cout << arr << endl;          //prints address of first block
+cout << *arr << endl;         //prints value at first block
+
+//pointer arithmetic
+cout << *(arr + 1) << endl;    //prints value at second block
+cout << *(arr + 2) << endl;    //prints value at third block
+
+//increment and decrement
+ cout << arr << endl;        //prints address of first block
+ cout << arr+1 << endl;    //prints address of second block
+ cout << arr+2 << endl;    //prints address of third block
+
 
     return 0;
 
